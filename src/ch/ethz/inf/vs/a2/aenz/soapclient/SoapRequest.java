0735bd9c7462d3f3b49b43fc7da3e4f26c209351
aenz <andi_enz@msn.com>
2014-10-12 12:11:14 +0200

    getting newest version

---
+++ src/ch/ethz/inf/vs/a2/aenz/soapclient/SoapRequest.java
@@ -0,0 +1,13 @@
+package ch.ethz.inf.vs.a2.aenz.soapclient;
+
+import ch.ethz.inf.vs.a2.aenz.http.Requester;
+
+public class SoapRequest implements Requester{
+
+	@Override
+	public String executeRequest() throws NullPointerException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
