7699ed4a50a16c5a3fe653e12ad668aa900bbeab
aenz <andi_enz@msn.com>
2014-10-13 11:33:26 +0200

    changed added soap argument

--- src/ch/ethz/inf/vs/a2/aenz/soapclient/SoapRequest.java
+++ src/ch/ethz/inf/vs/a2/aenz/soapclient/SoapRequest.java
@@ -1,6 +1,7 @@
 package ch.ethz.inf.vs.a2.aenz.soapclient;
 
 import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.PropertyInfo;
 import org.ksoap2.serialization.SoapObject;
 import org.ksoap2.serialization.SoapSerializationEnvelope;
 import org.ksoap2.transport.HttpTransportSE;
@@ -14,10 +15,11 @@ public class SoapRequest implements Requester, RemoteServerConfiguration{
 	
 	private static final String TAG = "SoapRequest";
 
-	private final String URL = "http://" + HOST + ":" + REST_PORT + "/SunSPOTWebServices/SunSPOTWebservice?wsdl";
+	private final String URL = "http://" + HOST + ":" + SOAP_PORT + "/SunSPOTWebServices/SunSPOTWebservice?wsdl";
 	private final String NAMESPACE = "http://webservices.vslecture.vs.inf.ethz.ch/";
-	private final String METHOD_NAME = "getDiscoveredSpots";
+	private final String METHOD_NAME = "getSpot";
 	private final String ARG = "Spot3";
+	private final String ARG_NAME = "parameters";
 	
 	private SoapObject request;
 	private SoapObject response;
@@ -25,7 +27,13 @@ public class SoapRequest implements Requester, RemoteServerConfiguration{
 	
 	public SoapRequest() {
 		request = new SoapObject(NAMESPACE, METHOD_NAME);
-//		request.addProperty("getSpot", ARG);
+		
+		PropertyInfo property = new PropertyInfo();
+		property.setNamespace(NAMESPACE);
+		property.setName(ARG_NAME);
+		property.setValue(ARG);
+		
+		request.addProperty(property);
 		
 		soapEnv = new SoapSerializationEnvelope(SoapEnvelope.VER11); //no idea what version is required
 		soapEnv.setOutputSoapObject(request);
@@ -40,16 +48,16 @@ public class SoapRequest implements Requester, RemoteServerConfiguration{
 		try{
 
 			
-			Log.d(TAG,"attempting http call");
+			Log.d(TAG,"attempting http call to: " + URL);
 			androidHttpTransport.call(NAMESPACE + METHOD_NAME, soapEnv);
 			
 			Log.d(TAG, "call was successfull");
 			
-			response = (SoapObject) soapEnv.bodyIn;
-			return (String) soapEnv.bodyIn;
+			return soapEnv.bodyIn.toString();
 		} catch (Exception e){
 			Log.d(TAG, "HTTP REQUEST:\n" + androidHttpTransport.requestDump);
 			Log.d(TAG, "HTTP RESPONSE:\n" + androidHttpTransport.responseDump);
+			e.printStackTrace();
 			return "EXCEPTION: http call failed";			
 		}
 	}
