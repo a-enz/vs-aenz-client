9dadd22bed046da6d64df2c253a9f096bfeb031f
aenz <andi_enz@msn.com>
2014-10-13 10:30:44 +0200

    merge

--- src/ch/ethz/inf/vs/a2/aenz/soapclient/SoapRequest.java
+++ src/ch/ethz/inf/vs/a2/aenz/soapclient/SoapRequest.java
@@ -15,9 +15,8 @@ public class SoapRequest implements Requester, RemoteServerConfiguration{
 	private static final String TAG = "SoapRequest";
 
 	private final String URL = "http://" + HOST + ":" + REST_PORT + "/SunSPOTWebServices/SunSPOTWebservice?wsdl";
-	private final String SOAP_ACTION = "http://webservices.vslecture.vs.inf.ethz.ch/getSpot";
 	private final String NAMESPACE = "http://webservices.vslecture.vs.inf.ethz.ch/";
-	private final String METHOD_NAME = "getSpot";
+	private final String METHOD_NAME = "getDiscoveredSpots";
 	private final String ARG = "Spot3";
 	
 	private SoapObject request;
@@ -26,9 +25,9 @@ public class SoapRequest implements Requester, RemoteServerConfiguration{
 	
 	public SoapRequest() {
 		request = new SoapObject(NAMESPACE, METHOD_NAME);
-		request.addProperty("getSpot", ARG);
+//		request.addProperty("getSpot", ARG);
 		
-		soapEnv = new SoapSerializationEnvelope(SoapEnvelope.VER10); //no idea what version is required
+		soapEnv = new SoapSerializationEnvelope(SoapEnvelope.VER11); //no idea what version is required
 		soapEnv.setOutputSoapObject(request);
 		soapEnv.dotNet = true;
 	}
@@ -36,18 +35,21 @@ public class SoapRequest implements Requester, RemoteServerConfiguration{
 	
 	@Override
 	public String executeRequest() throws NullPointerException {
+			HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
+			androidHttpTransport.debug = true; //capture raw data in logcat
 		try{
 
-			HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
 			
 			Log.d(TAG,"attempting http call");
-			androidHttpTransport.call(SOAP_ACTION, soapEnv);
+			androidHttpTransport.call(NAMESPACE + METHOD_NAME, soapEnv);
 			
-			Log.d(TAG,"http call done");
+			Log.d(TAG, "call was successfull");
+			
 			response = (SoapObject) soapEnv.bodyIn;
 			return (String) soapEnv.bodyIn;
 		} catch (Exception e){
-			Log.d(TAG, soapEnv.bodyOut.toString());
+			Log.d(TAG, "HTTP REQUEST:\n" + androidHttpTransport.requestDump);
+			Log.d(TAG, "HTTP RESPONSE:\n" + androidHttpTransport.responseDump);
 			return "EXCEPTION: http call failed";			
 		}
 	}
